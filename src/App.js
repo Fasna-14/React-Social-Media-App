b6e3aefd69c5ff4f1a1a9af944512884494bbefe
Fasna-14 <fahabfasna@gmail.com>
2023-10-24 07:29:23 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./Footer";
+import Header from "./Header";
+import Home from "./Home";
+import Missing from "./Missing";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+import Nav from "./Nav";
+import About from "./About";
+import EditPost from "./EditPost"
+import { Routes, Route} from "react-router-dom";
+import { DataProvider } from "./context/DataContext";
+
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DataProvider>
+        <Header title="My First Social Media App"/>
+        <Nav/>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/post">
+            <Route index
+              element={<NewPost/>} />
+
+            <Route path=":id" 
+              element={<PostPage/>} />
+          </Route>
+          <Route path="/edit/:id" element={<EditPost/>} />
+          <Route path="/about" element ={<About />} />
+          <Route path="*" element={<Missing />} />
+        </Routes>
+        <Footer />
+      </DataProvider>
     </div>
   );
 }
 
 export default App;
+
+
+{/* <nav>
+<ul>
+  <li><Link to="/"> Home </Link></li>
+  <li><Link to="/about"> About </Link></li>
+  <li><Link to="/newPost"> NewPost </Link></li>
+  <li><Link to="/postPage"> PostPage </Link></li>
+</ul>
+</nav>
+<Routes>
+<Route path="/" element={<Home />}/>
+<Route path="/about" element={<About />}/>
+<Route path="/newPost" element={<NewPost />}/>
+<Route path="/postPage" element={<PageLayout />}>
+  <Route index element={<PostPage />}/>
+  <Route path=":id" element={<Post />}/>
+  <Route path="newPost" element={<NewPost />}/>
+</Route>
+<Route path="*" element={<Missing />}/>
+</Routes> */}
---
+++ src/PageLayout.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
+import NewPost from './NewPost'
+
+const PageLayout = () => {
+  return (
+    <>
+        <Link to="/postPage/1"> Post 1 </Link>
+        <br />
+        <Link to="/postPage/2"> Post 2 </Link>
+        <br />
+        <Link to="/postPage/3"> Post 3 </Link>
+        <br />
+        <Link to="/postPage/newPost"> {<NewPost />} </Link>
+        <Outlet />
+    </>
+  )
+}
+
+export default PageLayout
